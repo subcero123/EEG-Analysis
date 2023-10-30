ec3893bb8e43a662c58446c5b11904f85878ec88
subcero123 <subcero139@hotmail.com>
2023-10-30 14:56:10 -0600

    Todo

--- AnalisisFFT/filtradoCSV.py
+++ AnalisisFFT/filtradoCSV.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 # Leer el archivo CSV
-df = pd.read_csv('../Estimulo/AURA_RAW___2023-09-26___11;15;10.csv')
+df = pd.read_csv('../E2023-09-11___11;42;36/AURA_RAW___2023-09-11___11;42;36.csv')
 print(df)
 
 # Extraer la parte de fecha y hora del formato existente
@@ -16,3 +16,19 @@ df['Time and date'] = (df['Time and date'] - df['Time and date'].iloc[0]).dt.tot
 
 # Guarda el DataFrame modificado de vuelta en un nuevo archivo CSV
 df.to_csv('RAWEstimulo-2023-09-11-11:15:10.csv', index=False)
+
+""" 
+Archivo
+Promedio de cada 500 filas; 
+Desviacion estandar
+Mediana
+Rango Intercuartil
+Min
+Max
+
+Aura FFT
+vs 
+Mia FFT
+
+Grafica cada punto resume 200 filas (promedio) (Mio vs Hector)
+"""
